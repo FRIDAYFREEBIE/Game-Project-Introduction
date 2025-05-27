0fc810f3ac69f1b9541ea6c584bc39492f52e537
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2025-05-27 14:39:36 +0900

    Bug Fix

--- Game-Project-Introduction/Assets/01_Scripts/Camera/CameraFollow.cs
+++ Game-Project-Introduction/Assets/01_Scripts/Camera/CameraFollow.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [Header("플레이어")]
-    public Transform target;
+  [Header("플레이어")]
+  public Transform target;
 
-    [Header("카메라 이동 속도")]
-    public float followSpeed = 7f;
+  [Header("카메라 세팅")]
+  public float followSpeed = 7f;
+  public float offsetY = 0f;
 
-    private void LateUpdate()
-    {
-      Vector3 targetPosition = new Vector3(target.position.x, target.position.y + 2.5f, -10f);
-      transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
-    }
+  private void LateUpdate()
+  {
+    Vector3 targetPosition = new Vector3(target.position.x, target.position.y + offsetY, -10f);
+    transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+  }
 }
